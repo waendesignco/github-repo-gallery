9ece777bc817eb5b33d31805bc531c666bedaeb5
ryoko-y <wagarden6@gmail.com>
2021-08-05 10:49:31 -0400

    Add fetched repos from Github API

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 const overview = document.querySelector (".overview");
 //Guthub username
 const username = "waendesignco";
+//Select the repos list
+const repoList = document.querySelector (".repo-list");
 
 //Fetch API jSON data
 const gitUserInfo = async function() {
@@ -29,6 +31,23 @@ const displayUserInfo = function (data) {
          <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
        </div> `;
        overview.append(div);
+       gitRepos();
 };
 
+//Fetch the repos
+const gitRepos = async function() {
+    const res = await fetch (`https://api.github.com/users/${username}/repos?sort=updated?per_pase=100`);
+    const repoData = await res.json();
+    console.log(repoData);
+    displayRepos(repoData); //pass jSON response data
+};
 
+//Display info about the repos
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList. add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+};
